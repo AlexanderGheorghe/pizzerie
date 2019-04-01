2d90d84c20a237cf7dc80d30c3c8d1389d25e2e6
Alex G <alexandru.gheorghe@my.fmi.unibuc.ro>
2019-04-01 17:21:05 +0300

    implemented RepositoryConfig

--- src/pizzeria/configuration/RepositoryConfig.java
+++ src/pizzeria/configuration/RepositoryConfig.java
@@ -6,4 +6,24 @@ public class RepositoryConfig {
     private final CustomerRepository customerRepository = new CustomerRepositoryImpl();
     private final PizzaRepository pizzaRepository = new PizzaRepositoryImpl();
     private final ReservationRepository reservationRepository = new ReservationRepositoryImpl();
+    private static RepositoryConfig instance = new RepositoryConfig();
+
+    public CustomerRepository getCustomerRepository() {
+        return customerRepository;
+    }
+
+    public PizzaRepository getPizzaRepository() {
+        return pizzaRepository;
+    }
+
+    public ReservationRepository getReservationRepository() {
+        return reservationRepository;
+    }
+
+    public static RepositoryConfig getInstance() {
+        return instance;
+    }
+
+    private RepositoryConfig() {
+    }
 }
